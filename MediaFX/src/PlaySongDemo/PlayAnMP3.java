c4e990e0f4a1399cf47a1a5751663584fc84b6c7
Minyoung Lee <min@Minyoungui-MacBookAir.local>
2023-03-01 20:57:08 -0700

    Made small changes to PlayOneMP3.java

--- MediaFX/src/PlaySongDemo/PlayAnMP3.java
+++ MediaFX/src/PlaySongDemo/PlayAnMP3.java
@@ -28,7 +28,8 @@ public class PlayAnMP3 extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     BorderPane pane = new BorderPane();
-    String path = "songfiles/Capture.mp3";
+    String path = "MediaFX/songFiles/SwingCheese.mp3"; // I changed a path!  It works now!  
+    												// I changed a song too
     pane.setCenter( new Label(path));
     playASong(path);
     // Put the pane in a sized Scene and show the GUI
